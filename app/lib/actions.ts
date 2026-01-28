a2079cea6fcc575d0b2accbb67db2093650ed646
Lisandra Cruz <lisansouza@gmail.com>
2026-01-28 15:30:17 -0300

    feat: cap 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,8 +4,31 @@ import z from "zod";
 import { revalidatePath } from 'next/cache';
 import postgres from 'postgres';
 import { redirect } from "next/dist/client/components/navigation";
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
+
 
 const FormSchema = z.object({
   id: z.string(),
@@ -73,22 +96,22 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 // ...
 
 export async function updateInvoice(id: string, formData: FormData) {
-const validatedFields = UpdateInvoice.safeParse({
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: 'Missing Fields. Failed to Update Invoice.',
     };
   }
- 
+
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
- 
+
   try {
     await sql`
       UPDATE invoices
@@ -98,7 +121,7 @@ const validatedFields = UpdateInvoice.safeParse({
   } catch (error) {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
- 
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
